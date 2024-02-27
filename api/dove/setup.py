039db8a598707d4bd15a4bcfc661f246062b07ee
hatsch <hatsch@servus.at>
2024-02-27 09:02:20 +0100

    Move frontend to seperate repo.

--- api/dove/setup.py
+++
@@ -1,5 +0,0 @@
-# inside setup.py - minimal stub:
-
-from setuptools import setup
-if __name__ == '__main__':
-    setup()
--- api/helpers.py
+++
@@ -1,8 +0,0 @@
-
-# generates unique IDs for DTOS
-
-def generateId(prefix="", start=1):
-    counter = start
-    while True:
-        yield f"{prefix}{counter}"
-        counter += 1
